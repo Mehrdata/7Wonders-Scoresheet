7beb4f1fa355b57c9dc56dd8b43202af8b206b12
Mehrdata <72032720+Mehrdata@users.noreply.github.com>
2023-03-29 14:07:34 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+let sum1 = document.getElementById("sump1")
+let sum2 = document.getElementById("sump2")
+let sum3 = document.getElementById("sump3")
+let sum4 = document.getElementById("sump4")
+let sum5 = document.getElementById("sump5")
+let sum6 = document.getElementById("sump6")
+let sum7 = document.getElementById("sump7")
+
+
+function getVal() {
+    let war1 = document.querySelector('#war-1').value;
+    let money1 = document.querySelector('#money-1').value;
+    let wonders1 = document.querySelector('#wonders-1').value;
+    let blue1 = document.querySelector('#blue-1').value;
+    let yellow1 = document.querySelector('#yellow-1').value;
+    let purple1 = document.querySelector('#purple-1').value;
+    let green1 = document.querySelector('#green-1').value;
+    let sigma1 = Number(war1)+Number(money1)+Number(wonders1)+Number(blue1)+Number(yellow1)+Number(purple1)+Number(green1)
+    sum1.textContent = sigma1
+  }
+
+  function getVal2() {
+    let war2 = document.querySelector('#war-2').value;
+    let money2 = document.querySelector('#money-2').value;
+    let wonders2 = document.querySelector('#wonders-2').value;
+    let blue2 = document.querySelector('#blue-2').value;
+    let yellow2 = document.querySelector('#yellow-2').value;
+    let purple2 = document.querySelector('#purple-2').value;
+    let green2 = document.querySelector('#green-2').value;
+    let sigma2 = Number(war2)+Number(money2)+Number(wonders2)+Number(blue2)+Number(yellow2)+Number(purple2)+Number(green2)
+    sum2.textContent = sigma2
+  }
+
+  function getVal3() {
+    let war3 = document.querySelector('#war-3').value;
+    let money3 = document.querySelector('#money-3').value;
+    let wonders3 = document.querySelector('#wonders-3').value;
+    let blue3 = document.querySelector('#blue-3').value;
+    let yellow3 = document.querySelector('#yellow-3').value;
+    let purple3 = document.querySelector('#purple-3').value;
+    let green3 = document.querySelector('#green-3').value;
+    let sigma3 = Number(war3)+Number(money3)+Number(wonders3)+Number(blue3)+Number(yellow3)+Number(purple3)+Number(green3)
+    sum3.textContent = sigma3
+  }
+
+  function getVal4() {
+    let war4 = document.querySelector('#war-4').value;
+    let money4 = document.querySelector('#money-4').value;
+    let wonders4 = document.querySelector('#wonders-4').value;
+    let blue4 = document.querySelector('#blue-4').value;
+    let yellow4 = document.querySelector('#yellow-4').value;
+    let purple4 = document.querySelector('#purple-4').value;
+    let green4 = document.querySelector('#green-4').value;
+    let sigma4 = Number(war4)+Number(money4)+Number(wonders4)+Number(blue4)+Number(yellow4)+Number(purple4)+Number(green4)
+    sum4.textContent = sigma4
+  }
+
+
+  function getVal5() {
+    let war5 = document.querySelector('#war-5').value;
+    let money5 = document.querySelector('#money-5').value;
+    let wonders5 = document.querySelector('#wonders-5').value;
+    let blue5 = document.querySelector('#blue-5').value;
+    let yellow5 = document.querySelector('#yellow-5').value;
+    let purple5 = document.querySelector('#purple-5').value;
+    let green5 = document.querySelector('#green-5').value;
+    let sigma5 = Number(war5)+Number(money5)+Number(wonders5)+Number(blue5)+Number(yellow5)+Number(purple5)+Number(green5)
+    sum5.textContent = sigma5
+  }
+
+  function getVal6() {
+    let war6 = document.querySelector('#war-6').value;
+    let money6 = document.querySelector('#money-6').value;
+    let wonders6 = document.querySelector('#wonders-6').value;
+    let blue6 = document.querySelector('#blue-6').value;
+    let yellow6 = document.querySelector('#yellow-6').value;
+    let purple6 = document.querySelector('#purple-6').value;
+    let green6 = document.querySelector('#green-6').value;
+    let sigma6 = Number(war6)+Number(money6)+Number(wonders6)+Number(blue6)+Number(yellow6)+Number(purple6)+Number(green6)
+    sum6.textContent = sigma6
+  }
+
+  function getVal7() {
+    let war7 = document.querySelector('#war-7').value;
+    let money7 = document.querySelector('#money-7').value;
+    let wonders7 = document.querySelector('#wonders-7').value;
+    let blue7 = document.querySelector('#blue-7').value;
+    let yellow7 = document.querySelector('#yellow-7').value;
+    let purple7 = document.querySelector('#purple-7').value;
+    let green7 = document.querySelector('#green-7').value;
+    let sigma7 = Number(war7)+Number(money7)+Number(wonders7)+Number(blue7)+Number(yellow7)+Number(purple7)+Number(green7)
+    sum7.textContent = sigma7
+  }
+
